85a9d051eca6bd3433f4676a954936aa5447718c
Hiromi Takahashi <hiromix222@gmail.com>
2022-04-30 09:49:02 +0900

    モデル返却のケース追加

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Optional
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item):
+    return item
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id, "item_price": item.price}
--- readme.md
+++ readme.md
@@ -2,3 +2,9 @@
 
 公式サイト参考
 https://fastapi.tiangolo.com/ja/
+
+自動対話型の API ドキュメント
+http://127.0.0.1:8000/docs
+
+代替の API ドキュメント
+http://127.0.0.1:8000/redoc
